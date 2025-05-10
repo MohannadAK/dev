5e668893a2463ff9a46697b6dbbb1323477c6a5c
MohannadAK <mohannad.a.karim@gmail.com>
2025-05-10 21:18:16 +0300

    automated dev commit

--- env/.config/nvim/lua/theprimeagen/lazy/treesitter.lua
+++ env/.config/nvim/lua/theprimeagen/lazy/treesitter.lua
@@ -7,7 +7,7 @@ return {
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
-                "jsdoc", "bash",
+                "jsdoc", "bash","python","go","cpp",
             },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -46,7 +46,7 @@ return {
                 -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
                 -- Using this option may slow down your editor, and you may see some duplicate highlights.
                 -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = { "markdown" },
+                additional_vim_regex_highlighting = true,
             },
         })
 
